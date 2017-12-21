9202931125de35bcc128313095d2a03522ffd3fa
niamurrell <niamurrell@users.noreply.github.com>
2017-12-20 20:48:02 -0800

    Update Thing model

--- models/userThing.js
+++ models/userThing.js
@@ -14,14 +14,4 @@ userThingSchema.virtual("useCount").get(function() {
   return this.usageDates.length;
 })
 
-// Fix purchasePrice to 2 decimal places
-userThingSchema.path("purchasePrice").get(function(num) {
-  return (num).toFixed(2);
-});
-
-// Fix currentValue to 2 decimal places
-userThingSchema.path("currentValue").get(function(num) {
-  return (num);
-});
-
 module.exports = mongoose.model("UserThing", userThingSchema);
